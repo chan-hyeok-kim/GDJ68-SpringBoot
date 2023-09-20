cd9bc07bee3ffce664963e73fa2e285b81391a41
Hamelen <daiolous@gmail.com>
2023-09-20 17:43:16 +0900

    save

--- src/main/java/com/ham/main/board/notice/NoticeService.java
+++ src/main/java/com/ham/main/board/notice/NoticeService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ham.main.board.BoardService;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@Transactional(rollbackFor = Exception.class)
 public class NoticeService implements BoardService {
     
 	@Autowired
