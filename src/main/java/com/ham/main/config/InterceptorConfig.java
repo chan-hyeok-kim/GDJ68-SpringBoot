5a6373a63d6f905eafc2e04207df600a0068193a
Hamelen <daiolous@gmail.com>
2023-09-21 16:00:39 +0900

    save

--- src/main/java/com/ham/main/config/InterceptorConfig.java
+++ src/main/java/com/ham/main/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.ham.main.interceptors.TestInterceptor;
 
@@ -14,11 +15,19 @@ public class InterceptorConfig implements WebMvcConfigurer {
 	@Autowired
 	private TestInterceptor testInterceptor;
 	
+	@Autowired
+	private LocaleChangeInterceptor localeChangeInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addInterceptor(testInterceptor)
-		        .addPathPatterns("/notice/list");
+		        .addPathPatterns("/notice/list")
+		        ;
+		
+		registry.addInterceptor(localeChangeInterceptor)
+		        .addPathPatterns("/**")
+		        ;
 		
 	}
 	
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!-- JSP에서 properties이 메세지를 사용할 수 있도록 하는 API -->
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,7 +21,8 @@
 				<c:import url="/WEB-INF/views/layout/topbar.jsp"></c:import>
 
 				<div class="container-fluid">
-				
+				    <h1>Welcome : <spring:message code="hello"></spring:message></h1>
+				    <h1><spring:message code="hi"></spring:message> </h1>
 				</div>
 
 			</div>
